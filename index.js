d3eeedb8151c3f56f35e5fe7916cf43747c4f662
Matt De Binion <debinionm@gmail.com>
2020-11-24 14:57:06 -0800

    Prefix, owner, and invite.

---
+++ index.js
@@ -0,0 +1,9 @@
+const { CommandoClient } = require('discord.js-commando');
+const path = require('path');
+
+// constant client variable
+const client = new CommandoClient({
+    commandPrefix: '/',
+    owner: '231635743246778369',
+    invite: 'https://discord.gg/yPVtt8D',
+});
